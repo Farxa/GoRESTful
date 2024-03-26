17b81b26fdada346d4391f74573c166cb80ea23b
farxa <r.abualhassan@gmail.com>
2024-03-26 16:53:44 +0100

    Add svelte client

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    let users = [];
+  
+    async function fetchUsers() {
+      const response = await fetch('http://localhost:8080/users');
+      users = await response.json();
+    }
+  
+    fetchUsers();
+  </script>
+  
+  <main>
+    <h1>Users</h1>
+    {#each users as user}
+      <div>
+        <p>Username: {user.username}</p>
+        <p>Email: {user.email}</p> 
+      </div>
+    {/each}
+  </main>
+  
